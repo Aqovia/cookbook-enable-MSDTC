e9e9959b465e7ea11a7fc024bbf2f7aae7359a60
ETX\vladimiro <vladimiro@interxion.com>
2017-01-20 12:36:49 +0000

    all values are using attributes now

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,14 @@
 #lowest port number of the range we are going to open
-default['fromport'] = '49512'
+default['msdtc']['fromport'] = '49512'
 #highest port number of the range we are going to open
-default['toport'] = '65535'
+default['msdtc']['toport'] = '65535'
+
+default['msdtc']['NetworkDtcAccessTransactions'] = '1'
+default['msdtc']['NetworkDtcAccess'] = '1'
+default['msdtc']['NetworkDtcAccessClients'] = '1'
+default['msdtc']['NetworkDtcAccessInbound'] = '1'
+default['msdtc']['NetworkDtcAccessOutbound'] = '1'
+default['msdtc']['NetworkDtcAccessTip'] = '1'
+default['msdtc']['PortsInternetAvailable'] = 'Y'
+default['msdtc']['UseInternetPorts'] = 'Y'
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,26 +9,26 @@
 
 
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSDTC\Security' do
-  values [{:name => 'NetworkDtcAccessTransactions', :type => :dword, :data => 1},
-          {:name => 'NetworkDtcAccess', :type => :dword, :data => 1},
-          {:name => 'NetworkDtcAccessClients', :type => :dword, :data => 1},
-          {:name => 'NetworkDtcAccessInbound', :type => :dword, :data => 1},
-          {:name => 'NetworkDtcAccessOutbound', :type => :dword, :data => 1},
-          {:name => 'NetworkDtcAccessTip', :type => :dword, :data => 1}
+  values [{:name => 'NetworkDtcAccessTransactions', :type => :dword, :data => node['msdtc']['NetworkDtcAccessTransactions']},
+          {:name => 'NetworkDtcAccess', :type => :dword, :data => node['msdtc']['NetworkDtcAccess']},
+          {:name => 'NetworkDtcAccessClients', :type => :dword, :data => node['msdtc']['NetworkDtcAccessClients']},
+          {:name => 'NetworkDtcAccessInbound', :type => :dword, :data => node['msdtc']['NetworkDtcAccessInbound']},
+          {:name => 'NetworkDtcAccessOutbound', :type => :dword, :data => node['msdtc']['NetworkDtcAccessOutbound']},
+          {:name => 'NetworkDtcAccessTip', :type => :dword, :data => node['msdtc']['NetworkDtcAccessTip']}
          ]
-  action :create
+  action :create_if_missing
 end
 
 windows_firewall_rule 'Sql Server DTC' do
-	localport node['fromport']'..'node['toport']
+	localport node['msdtc']['fromport']..node['msdtc']['toport']
 	protocol 'TCP'
 	firewall_action :allow
 end
 
 registry_key 'HKEY_LOCAL_MACHINE\Software\Microsoft\Rpc\Internet' do
-  values [{:name => 'Ports', :type => :REG_MULTI_SZ, :data => node['fromport']-node['toport']},
-          {:name => 'PortsInternetAvailable', :type => :REG_SZ, :data => Y},
-          {:name => 'UseInternetPorts', :type => :REG_SZ, :data => Y}          
+  values [{:name => 'Ports', :type => :REG_MULTI_SZ, :data => node['msdtc']['fromport']-node['msdtc']['toport']},
+          {:name => 'PortsInternetAvailable', :type => :REG_SZ, :data => node['msdtc']['PortsInternetAvailable']},
+          {:name => 'UseInternetPorts', :type => :REG_SZ, :data => node['msdtc']['UseInternetPorts']}
          ]
-  action :create
+  action :create_if_missing
 end
